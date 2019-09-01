717993f964bbf20f05b4bed1aa397e25751fa986
Gabriel <pedromurillo96@gmail.com>
2019-09-01 13:49:45 -0700

    html and js major updates

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,23 @@
+var menu = [
+  {
+    item: "hamachi yellowtail",
+    price: "10"
+  }
+];
+
+$("#submit").on("click", function(e){
+  e.preventDefault();
+  console.log("order submitted");
+  var tamago_egg = $('input[name=tamago_egg]').val();
+  var california_roll = $('input[name=california_roll]').val();
+  var cart = {
+    tamago_egg, 
+    california_roll,
+  }
+  
+  console.log(cart);
+});
+
 //This JQuery is to make the quantity buttons work.
 jQuery(document).ready(function(){
   // This button will increment the value
@@ -6,6 +26,7 @@ jQuery(document).ready(function(){
       e.preventDefault();
       // Get the field name
       fieldName = $(this).attr('field');
+      console.log(fieldName);
       // Get its current value
       var currentVal = parseInt($('input[name='+fieldName+']').val());
       // If is not undefined
@@ -36,4 +57,20 @@ jQuery(document).ready(function(){
   });
 });
 
+
+
+
+
+// var cart = {
+//   hamachi_roll: 2,
+//   california_roll: 3
+// };
+
+// var results = Object.values(cart);
+// results
+// var total = results.reduce((a, b) => a + b)
+// total
+
+// var times10 = results.map(item => item * 10);
+// times10
 
