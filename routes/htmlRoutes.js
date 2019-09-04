80328eceeeb02d30217d2a01fa3eb336147884a4
mboslin <michael@boslin.com>
2019-09-04 12:28:17 -0700

    Updated .gitignore file.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,18 +16,18 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/landing.html"));
   });
 
-  app.get("/login", function(req, res) {
+  app.get("/menu", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "/menu.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/orders", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "/orders.html"));
   });
 
 };
